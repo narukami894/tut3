5170a0e0400371808ec68e18ea223ca37053eff2
narukami894 <s.yakushiji@icloud.com>
2017-05-25 18:33:11 +0900

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  before_action :set_user, except: %i[new create index destroy]
+
+  def new; end
+
+  def create; end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def index; end
+
+  def destroy; end
+
+  def following; end
+
+  def followers; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
-  def edit
-  end
-
-  def index
-  end
-
-  def show
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
