c1fe1955d2b48ef44674d26e9945f2a2cb21755d
narukami894 <s.yakushiji@icloud.com>
2017-05-25 16:26:10 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/help',    to: 'static_pages#help'
+  # 固定ページ
+  root   'static_pages#home'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/help',    to: 'static_pages#help'
+  # ログイン周り
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :account_activations, only: :edit
+  resources :password_resets, only: %i[new create edit update]
+
+  resources :users, except: %i[new create] do
+    member do
+      get 'following', 'followers'
+    end
+  end
+  resources :microposts, only: %i[create destroy]
+  resources :relationships, only: %i[create destroy]
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,31 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = 'Ruby on Rails Tutorial Sample App'
-  end
-
   test 'should get home' do
     get root_url
     assert_response :success
-    assert_select 'title', full_title('home')
+    assert_select 'title', full_title
   end
 
   test 'should get help' do
     get help_url
     assert_response :success
-    assert_select 'title', full_title('help')
+    assert_select 'title', full_title('Help')
   end
 
   test 'should get contact' do
     get contact_url
     assert_response :success
-    assert_select 'title', full_title('contact')
+    assert_select 'title', full_title('Contact')
   end
 
   test 'should get about' do
     get about_url
     assert_response :success
-    assert_select 'title', full_title('about')
+    assert_select 'title', full_title('About')
   end
 end
