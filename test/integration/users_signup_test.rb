cdd30b47d2f89e505a21768fdbf855238d32c50e
narukami894 <s.yakushiji@icloud.com>
2017-05-26 15:11:52 +0900

    done

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,23 +10,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid' do
     get signup_path
     assert_no_difference 'User.count' do
-      post signup_path, params: {user: {name: '', email: 's', password: 'o',
-                                                  password_confirmation: 's'}}
+      post signup_path, params: { user: { name: '', email: 's', password: 'o',
+                                          password_confirmation: 's' } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation > ul > li', count: 4
-
   end
 
   test 'valid' do
     get signup_path
     assert_difference 'User.count', 1 do
-      post signup_path, params: {user:{name:'jiro', email: 'ramen@jiro.com',
-                                        password:'foobar',
-                                        password_confirmation:'foobar'}}
+      post signup_path, params: { user: { name: 'jiro', email: 'ramen@jiro.com',
+                                          password: 'foobar',
+                                          password_confirmation: 'foobar' } }
     end
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
