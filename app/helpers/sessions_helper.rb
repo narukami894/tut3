1bd5704920516182764f7289659c30579929fca0
narukami894 <s.yakushiji@icloud.com>
2017-05-29 11:57:24 +0900

    done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,13 @@
 module SessionsHelper
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -23,6 +32,10 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  def current_user?(user)
+    user == current_user
   end
 
   def forget(user)
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,23 +1,85 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test 'should get new' do
     get signup_url
     assert_response :success
   end
 
-  # test 'should get edit' do
-  #   get edit_user_url
-  #   assert_response :success
-  # end
+  test 'should get show' do
+    get user_url(@user)
+    assert_response :success
+  end
 
   test 'should get index' do
+    log_in_as @user
     get users_url
     assert_response :success
   end
 
-  # test 'should get show' do
-  #   get user_url
-  #   assert_response :success
-  # end
+  test 'should redirect index when not logged in' do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test 'should get edit' do
+    log_in_as @user
+    get edit_user_url(@user)
+    assert_response :success
+  end
+
+  test 'should redirect edit when not logged in' do
+    get edit_user_url(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect edit when wrong user accesses' do
+    log_in_as @other_user
+    get edit_user_url(@user)
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_url(@user), params: { user: { name: 'hoge', email: 'fuga@piyo.com' } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when wrong user accesses' do
+    log_in_as @other_user
+    patch user_url(@user), params: { user: { name: 'hoge', email: 'fuga@piyo.com' } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_url(@other_user), params: { user: { password: 'password',
+                                                   password_confirmation: 'password',
+                                                   admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@other_user)
+    end
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
